cc09c5675c6e4f2b36bcd13dbe1e96c704e4d595
greenhalghdan <49040634+greenhalghdan@users.noreply.github.com>
2023-05-29 20:33:35 +0100

    etch a sketch 2

--- main.py
+++ main.py
@@ -12,10 +12,16 @@ def move_counter_clockwise():
 def move_clockwise():
     tim.circle(-25, 20)
 
+def clear_screen():
+    tim.clear()
+    tim.penup()
+    tim.home()
+    tim.pendown()
 screen = Screen()
 screen.listen()
 screen.onkey(key="w", fun=move_forwards)
 screen.onkey(key="s", fun=move_backwards)
 screen.onkey(key="a", fun=move_counter_clockwise)
 screen.onkey(key="d", fun=move_clockwise)
+screen.onkey(key="c", fun=clear_screen)
 screen.exitonclick()
